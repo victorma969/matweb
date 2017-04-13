b28701dc6fcc451bc942c3b6fb3cdf2a9c3a1ef2
Gabriel <gabryelll.carvalho@gmail.com>
2017-04-13 11:09:43 -0300

    criação do model e controll do Registro do usuario

---
+++ backend/Database/Controllers/Registro.py
@@ -0,0 +1,26 @@
+from Framework.BancoDeDados import BancoDeDados
+from Database.Models.Registro import Registro as ModelRegistro
+
+
+class Registro(object):
+		
+	def pegarRegistro(self, condicao, valores, inicio=0, quantidade=0):
+		registro = []
+		for registro in BancoDeDados().consultarMultiplos("SELECT * FROM registro WHERE %s" % (condicao), valores):
+			registro.append(ModelRegistro(registro))
+		return registro
+	
+	def pegarRegistro(self, condicao, valores):
+		return ModelRegistro(BancoDeDados().consultarUnico("SELECT * FROM registro WHERE %s" % (condicao), valores))
+	
+	def inserirRegistro(self, registro):
+		BancoDeDados().executar("INSERT INTO registro (id, token, idusuario, ip, status) VALUES (%s,%s,%s,%s,%s) RETURNING id", (registro.id, registro.token, registro.idusuario, registro.ip, registro.status))
+		registro.id = BancoDeDados().pegarUltimoIDInserido()
+		return registro
+		
+	def removerRegistro(self, registro):
+		BancoDeDados().executar("DELETE FROM registro WHERE id = %s", (str(registro.id)))
+		
+	def alterarRegistro(self, registro):
+		SQL = "UPDATE registro SET token = %s, idusuario = %s, ip = %s, status = %s WHERE id = %s"
+		BancoDeDados().executar(SQL, (registro.token, registro.idusuario, registro.ip, registro.status, registro.id))
--- backend/Database/Models/Registro.py
+++ backend/Database/Models/Registro.py
@@ -1,12 +1,36 @@
-from matweb import Matwebdb
-
-
 class Resgistro(object):
 		def __init__ (self.dados):
+		if dados is not None:
 			self.id = dados['id']
 			self.token = dados['token']
 			self.idusuario = dados['idsuario']
 			self.ip = dados['ip']
-			self.status dados['status']
+			self.status = dados['status']
 			pass
 		
+	def getId(self):
+		return self.id
+
+	def setToken(self,token):
+		self.token = token
+		
+	def getToken(self):
+		return self.token
+
+	def setIdusuario (self,idusuario):
+		self.idusuario = idusuario
+
+	def getIdusuario (self):
+		return self.idusuario
+		
+	def getIp (self):
+		return self.ip
+
+	def setIp(self,ip):
+		self.ip = ip
+
+	def getStatus(self):
+		return self.status
+
+	def setStatus(self, status):
+		self.status = status
