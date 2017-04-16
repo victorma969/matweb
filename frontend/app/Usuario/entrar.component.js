488f302d0d675c92f8cf98b42248b780cf866da8
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-16 10:15:27 -0300

    teste frontend

--- frontend/app/Usuario/entrar.component.js
+++ frontend/app/Usuario/entrar.component.js
@@ -2,10 +2,10 @@ angular.
   module('Usuario').
   component('usuarioEntrar', {
     template: 'Hello, {{$ctrl.user}}!',
-    controller: ['ApiUsuario','$httpProvider', function GreetUserController(ApiUsuario,$httpProvider) {
+    controller: ['ApiUsuario','$httpProvider', function GreetUserController(ApiUsuario,$http) {
       this.user = "Lucas";
       var resultado = ApiUsuario.Entrar({ "usuario":"00743723223", "senha":"Biscoito" },function() {
-       				$httpProvider.defaults.headers.common.Authorization = resultado.corpo.token;
+       				$http.defaults.headers.common.Authorization = resultado.corpo.token;
        				console.log(resultado.corpo.token)
    				} );
     }]
