173a31ad8448c7a144ecbdb915b6e2dd47c7f0d5
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-16 09:10:30 -0300

    teste frontend

--- frontend/app/app.config.js
+++ frontend/app/app.config.js
@@ -7,13 +7,6 @@ angular.
       $http.defaults.headers.common.Authorization = ""
       $http.defaults.headers.post = { 'Content-Type' : 'application/json; charset=UTF-8' }
 
-      $routeProvider.
-        when('/', {
-          template: '<site-home></site-home>'
-        }).
-        when('/login', {
-          template: '<usuario-login></usuario-login>'
-        }).
-        otherwise('/');
+      $routeProvider.otherwise('/');
     }
   ]);
