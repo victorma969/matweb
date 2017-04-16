23c4b869bbfdc5ce87c6d7b816fad0c048ad5e48
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-16 09:30:56 -0300

    teste frontend

--- frontend/app/Usuario/entrar.component.js
+++ frontend/app/Usuario/entrar.component.js
@@ -1,6 +1,6 @@
 angular.
   module('Usuario').
-  component('UsuarioEntrar', {
+  component('usuarioEntrar', {
     template: 'Hello, {{$ctrl.user.corpo.nome}}!',
     controller: ['ApiUsuario', function GreetUserController(ApiUsuario) {
       this.user = ApiUsuario.Entrar({ "usuario":"00743723223", "senha":"Biscoito" });
