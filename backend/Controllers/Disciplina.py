2d2ea1284625901c0b555754197e415bd1b8dc3b
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-16 07:59:44 -0300

    Corrigido erro

--- backend/Controllers/Disciplina.py
+++ backend/Controllers/Disciplina.py
@@ -3,7 +3,7 @@ from Framework.Controller import Controller
 from Database.Controllers.Disciplina import Disciplina as BDDisciplina
 from Models.Disciplina.RespostaListar import RespostaListar
 
-class Departamento(Controller):
+class Disciplina(Controller):
 
 	def Listar(pedido_listar):
 		return RespostaListar(BDDisciplina().pegarDisciplinas("WHERE id_departamento = %d, nome = %s LIMIT %d OFFSET %d",(pedido_listar.getIdDepartamento(),pedido_listar.getNome().replace(' ','%'),pedido_listar.getQuantidade(),(pedido_listar.getQuantidade()*pedido_listar.getPagina()))))
--- backend/Controllers/Horario.py
+++ backend/Controllers/Horario.py
@@ -3,7 +3,7 @@ from Framework.Controller import Controller
 from Database.Controllers.Horario import Horario as BDHorario
 from Models.Horario.RespostaListar import RespostaListar
 
-class Departamento(Controller):
+class Horario(Controller):
 
 	def Listar(pedido_listar):
 		return RespostaListar(BDHorario().pegarHorarios("",None))
