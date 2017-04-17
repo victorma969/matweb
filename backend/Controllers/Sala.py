fefabcaef776fcaa8a0eba203b7b33ec999e92f4
henrique1998 <henri.rnp@gmail.com>
2017-04-17 18:20:46 -0300

    Update Sala.py

--- backend/Controllers/Sala.py
+++ backend/Controllers/Sala.py
@@ -5,4 +5,4 @@ from Models.Sala.RespostaListar import RespostaListar
 class Sala(Controller):
 
       def Listar(self,pedido_listar):
-             		      return RespostaListar(BDSala().pegarSalas("WHERE id_predio = %s AND codigo = %s LIMIT %s OFFSET %s",(pedido_listar.getIdPredio(),"%".pedido_listar.getCodigo().replace(' ','%')."%",pedido_listar.getQuantidade(),(pedido_listar.getQuantidade()*pedido_listar.getPagina()))))
+             		      return RespostaListar(BDSala().pegarSalas("WHERE id_predio = %s AND codigo LIKE %s LIMIT %s OFFSET %s",(pedido_listar.getIdPredio(),"%".pedido_listar.getCodigo().replace(' ','%')."%",pedido_listar.getQuantidade(),(pedido_listar.getQuantidade()*pedido_listar.getPagina()))))
