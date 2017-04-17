a5015d7e46e64215559734e2166143635de7f760
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-17 09:45:39 -0300

    Frontend

---
+++ frontend/app/nucleo/menu-principal.component.js
@@ -0,0 +1,8 @@
+angular.
+  module('nucleo').
+  component('menuPrincipal', {
+    templateUrl: '/app/nucleo/menu-principal.template.html',
+    controller: [function Menu() {
+      
+    }]
+  });
