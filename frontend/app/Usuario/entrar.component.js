06fb9ec33992a6b67f8ec1ba199d7852133ec4b9
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-17 08:40:49 -0300

    Frontend

--- frontend/app/Usuario/entrar.component.js
+++ frontend/app/Usuario/entrar.component.js
@@ -2,12 +2,16 @@ angular.
   module('Usuario').
   component('usuarioEntrar', {
     templateUrl: '/app/Usuario/entrar.template.html',
-    controller: ['ApiUsuario','$http', function GreetUserController(ApiUsuario,$http) {
+    controller: ['ApiUsuario','$http', function Entrar(ApiUsuario,$http) {
       this.user = "Lucas";
       var resultado = ApiUsuario.Entrar({ "usuario":"00743723223", "senha":"Biscoito" },function() {
        				$http.defaults.headers.common.Authorization = resultado.corpo.token;
        				window.localStorage.setItem('token_de_acesso', resultado.corpo.token);
        				console.log(resultado.corpo.token)
    				} );
+      this.entrar = function()
+      {
+
+      }
     }]
   });
