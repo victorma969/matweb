400d3e6525a1284bbff0a2e72cf320f5217f1265
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-17 18:41:26 -0300

    Auto-committing 'Usuario.py'

--- backend/Controllers/Usuario.py
+++ backend/Controllers/Usuario.py
@@ -13,10 +13,10 @@ class Usuario(Controller):
 	def temAcesso(metodo,perfis):
 			def metodo_com_acesso(self,pedido)
 				usuario = Usuario.__getUsuarioPeloToken(pedido.variaveis_do_ambiente["AUTHORIZATION"])
-				if():
-					ErroNoHTTP(403,"Acesso Negado!")
-				else
+				if usuario.getPerfil() in perfis:
 					return metodo(self,pedido,usuario)
+				else
+					ErroNoHTTP(403,"Acesso Negado!")
 		return metodo_com_acesso
 		
 	def Entrar(self,pedido_entrar):
