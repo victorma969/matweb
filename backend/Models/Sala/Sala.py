3f77cc9a13dad1e1fda76054f55695fbce5986f4
henrique1998 <henri.rnp@gmail.com>
2017-04-17 14:15:42 -0300

    Update Sala.py

--- backend/Models/Sala/Sala.py
+++ backend/Models/Sala/Sala.py
@@ -1,5 +1,5 @@
-class Predio(object):
-	def __init__(self,predio):
-		self.id = predio.getId()
-		self.codigo = predio.getCodigo()
+class Sala(object): 
+	def __init__(self,sala):
+		self.id = sala.getId()
+		self.codigo = sala.getCodigo()
     
