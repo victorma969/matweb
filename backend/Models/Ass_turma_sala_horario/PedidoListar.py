540478080421517747767d9a3ba505b1fdbd46ec
henrique1998 <henri.rnp@gmail.com>
2017-04-17 15:09:31 -0300

    Create PedidoListar.py

---
+++ backend/Models/Ass_turma_sala_horario/PedidoListar.py
@@ -0,0 +1,18 @@
+from Framework.Pedido import Pedido
+from Framework.ErroNoHTTP import ErroNoHTTP
+
+class PedidoListar(Pedido):
+
+	def __init__(self,variaveis_do_ambiente):
+		super(PedidoListar, self).__init__(variaveis_do_ambiente)
+		try:
+			self.pagina = self.corpo['pagina']
+			self.quantidade = self.corpo['quantidade']
+		except:
+			raise ErroNoHTTP(400)
+
+	def getPagina(self):
+		return self.pagina
+
+	def getQuantidade(self):
+		return self.quantidade
