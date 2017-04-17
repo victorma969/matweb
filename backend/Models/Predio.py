6066b07585209384e23d60fe4c363896ceafc9a2
henrique1998 <henri.rnp@gmail.com>
2017-04-17 10:41:55 -0300

    Create Predio.py

---
+++ backend/Models/Predio.py
@@ -0,0 +1,7 @@
+class Predio(object):
+	def __init__(self,prdio):
+		self.id = prdio.getId()
+		self.nome = predio.getNome()
+   	self.sigla = predio.getSigla()
+		self.latitude = predio.getLatitude()
+		self.longitude = predio.getLongitude()
