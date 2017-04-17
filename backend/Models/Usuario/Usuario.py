87760b86f1b68e25fe469beddf7e24d7d686bffd
henrique1998 <henri.rnp@gmail.com>
2017-04-17 18:53:18 -0300

    Create Usuario.py

---
+++ backend/Models/Usuario/Usuario.py
@@ -0,0 +1,8 @@
+class Usuario(object): 
+	def __init__(self,usuario):
+		self.id = usuario.getId()
+		self.matricula = usuario.getMatricula()
+    self.nome = usuario.getNome()
+    self.cpf = usuario.getCpf()
+    self.perfil = usuario.getPerfil()
+    self.senha = usuario.getSenha()
