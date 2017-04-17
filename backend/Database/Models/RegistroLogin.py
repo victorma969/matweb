c3c2a4beb771ffa8d72e40d44f8a7058101f2b1a
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-17 18:35:31 -0300

    Auto-committing 'RegistroLogin.py'

---
+++ backend/Database/Models/RegistroLogin.py
@@ -0,0 +1,36 @@
+class ResgistroLogin(object):
+		def __init__ (self.dados):
+		if dados is not None:
+			self.id = dados['id']
+			self.token = dados['token']
+			self.idusuario = dados['id_usuario']
+			self.ip = dados['ip']
+			self.entrada = dados['entrada']
+			pass
+		
+	def getId(self):
+		return self.id
+
+	def setToken(self,token):
+		self.token = token
+		
+	def getToken(self):
+		return self.token
+
+	def setId_usuario (self,id_usuario):
+		self.id_usuario = id_usuario
+
+	def getId_usuario (self):
+		return self.id_usuario
+		
+	def getIp (self):
+		return self.ip
+
+	def setIp(self,ip):
+		self.ip = ip
+
+	def getEntrada(self):
+		return self.entrada
+
+	def setEntrada(self, entrada):
+		self.entrada = entrada
