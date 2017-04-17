4600e49f19b512391796c301c41101c92a8369ba
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-04-17 13:35:09 -0300

    teste

--- frontend/app/Oferta/oferta.component.js
+++ frontend/app/Oferta/oferta.component.js
@@ -4,12 +4,15 @@ angular.
     templateUrl: '/app/Oferta/oferta.template.html',
     controller: ['ApiOferta', 'MatWebGlobals',function Entrar(ApiOferta,MatWebGlobals) {
       this.nome_disciplina = "";
+	this.disciplinas = [];
       this.pesquisar = function()
       {
        	ApiOferta.Listar({id_departamento: 95 , nome: this.nome_disciplina, pagina: 0, quantidade: 1000 },function(resultado) {
 		          this.disciplinas = resultado.corpo
+			console.log(this.disciplinas)
 		}, function(erro){
    			this.erro = erro.data.mensagem
+			console.log(this.erro)
    		} );
    	  }
     }]
