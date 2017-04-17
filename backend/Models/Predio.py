21da4596e03820c91810f2817dfcfa23a497fdc5
henrique1998 <henri.rnp@gmail.com>
2017-04-17 10:44:42 -0300

    Delete Predio.py

--- backend/Models/Predio.py
+++
@@ -1,7 +0,0 @@
-class Predio(object):
-	def __init__(self,prdio):
-		self.id = prdio.getId()
-		self.nome = predio.getNome()
-   	        self.sigla = predio.getSigla()
-		self.latitude = predio.getLatitude()
-		self.longitude = predio.getLongitude()
