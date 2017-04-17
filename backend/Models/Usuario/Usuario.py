d286e8a61ba908d500427187d89d1c01e6713d66
henrique1998 <henri.rnp@gmail.com>
2017-04-17 18:54:53 -0300

    Update Usuario.py

--- backend/Models/Usuario/Usuario.py
+++ backend/Models/Usuario/Usuario.py
@@ -2,7 +2,7 @@ class Usuario(object):
 	def __init__(self,usuario):
 		self.id = usuario.getId()
 		self.matricula = usuario.getMatricula()
-    self.nome = usuario.getNome()
-    self.cpf = usuario.getCpf()
-    self.perfil = usuario.getPerfil()
-    self.senha = usuario.getSenha()
+                self.nome = usuario.getNome()
+                self.cpf = usuario.getCpf()
+                self.perfil = usuario.getPerfil()
+                self.senha = usuario.getSenha()
