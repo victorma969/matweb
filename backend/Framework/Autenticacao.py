0802b94df3a663e6f3fe7eeeb2afd3e89b77a5a2
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-18 09:42:35 -0300

    Auto-committing 'Autenticacao.py'

--- backend/Framework/Autenticacao.py
+++ backend/Framework/Autenticacao.py
@@ -1,6 +1,9 @@
-from Database.Controllers.RegistroLogin import RegistroLogin
+from Database.Controllers.RegistroLogin import RegistroLogin as DBRegistroLogin
+from Database.Controllers.RegistroLogin import RegistroLogin as DBRegistroLogin
 from Database.Models.RegistroLogin import RegistroLogin as ModelRegistroLogin
+import uuid
 
 class Autenticacao(object):
 
-	
+	@staticmethod
+	def getUsuarioPeloTokenEIp(token,ip):
