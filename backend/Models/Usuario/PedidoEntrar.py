b7022c3bd2bdaa1e05fd121dce42caba0f1144f1
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-18 07:48:24 -0300

    Auto-committing 'PedidoEntrar.py'

--- backend/Models/Usuario/PedidoEntrar.py
+++ backend/Models/Usuario/PedidoEntrar.py
@@ -1,30 +1,18 @@
 from Framework.Pedido import Pedido
 from Framework.ErroNoHTTP import ErroNoHTTP
 
-class PedidoEntrar(Pedido):
+class PedidoCadastrar(Pedido):
 
 	def __init__(self,variaveis_do_ambiente):
-		super(PedidoEntrar, self).__init__(variaveis_do_ambiente)
+		super(PedidoCadastrar, self).__init__(variaveis_do_ambiente)
 		try:
-			self.nome = self.corpo['nome']
-			self.matricula = self.corpo['matricula']
-			self.cpf = self.corpo['cpf']
-			self.perfil = self.corpo['perfil']
+			self.usuario = self.corpo['usuario']
 			self.senha = self.corpo['senha']
 		except:
 			raise ErroNoHTTP(400)
 
-	def getNome(self):
-		return self.nome
+	def getLoginDoUsuario(self):
+		return self.usuario
 
-	def getMatricula(self):
-		return self.matricula
-
-	def getCpf(self):
-		return self.cpf
-
-	def getPerfil(self):
-		return self.perfil
-
-	def getSenha(self):
+	def getSenhaDoUsuario(self):
 		return self.senha
