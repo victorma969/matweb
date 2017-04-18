0029ff80389199d8618fd9c8ca7c6ec5dafec77d
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-18 08:00:18 -0300

    Auto-committing 'PedidoCadastrar.py'

--- backend/Models/Usuario/PedidoCadastrar.py
+++ backend/Models/Usuario/PedidoCadastrar.py
@@ -1,10 +1,10 @@
 from Framework.Pedido import Pedido
 from Framework.ErroNoHTTP import ErroNoHTTP
 
-class PedidoEntrar(Pedido):
+class PedidoCadastrar(Pedido):
 
 	def __init__(self,variaveis_do_ambiente):
-		super(PedidoEntrar, self).__init__(variaveis_do_ambiente)
+		super(PedidoCadastrar, self).__init__(variaveis_do_ambiente)
 		try:
 			self.nome = self.corpo['nome']
 			self.matricula = self.corpo['matricula']
