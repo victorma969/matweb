3813fc52ea54404ec0f953ae48204b58c08ba08d
Lucas Martins Alves <contato@lumal21.net.br>
2017-04-18 07:52:19 -0300

    Auto-committing 'RespostaEntrar.py'

--- backend/Models/Usuario/RespostaEntrar.py
+++ backend/Models/Usuario/RespostaEntrar.py
@@ -1,6 +1,7 @@
 from Framework.Resposta import Resposta
+from Models.Usuario.Usuario import Usuario as ModelUsuario
 
 class RespostaEntrar(Resposta):
 
 	def __init__(self,token=None,usuario=None):
-		self.corpo = { 'token' : token , 'usuario' : usuario }
+		self.corpo = { 'token' : token , 'usuario' : ModelUsuario(usuario) }
