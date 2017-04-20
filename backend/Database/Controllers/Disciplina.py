14fce67a1eea53219d66da61e2c3abe0e0d3d0db
Romulo Alcantara <romuloos7899@gmail.com>
2017-04-19 22:34:42 -0300

    Teste ignore case

--- backend/Database/Controllers/Disciplina.py
+++ backend/Database/Controllers/Disciplina.py
@@ -6,7 +6,7 @@ class Disciplina(object):
 	
 	def pegarDisciplinas(self, condicao, valores):
 		disciplinas = []
-		for disciplina in BancoDeDados().consultarMultiplos("SELECT * FROM disciplina WHERE nome LIKE (%s)" % (condicao), valores):
+		for disciplina in BancoDeDados().consultarMultiplos("SELECT FROM disciplina WHERE nome LIKE '%'" % (condicao), valores):
 			disciplinas.append(ModelDisciplina(disciplina))
 		return disciplinas
 	
