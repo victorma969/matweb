be168c7c231d28198256aa1c4c0af292c862eab7
henrique1998 <henri.rnp@gmail.com>
2017-04-25 23:30:29 -0300

    Update PedidoEditar.py

--- backend/Models/Disciplina/PedidoEditar.py
+++ backend/Models/Disciplina/PedidoEditar.py
@@ -8,7 +8,7 @@ class PedidoEditar(Pedido):
 		try:
 			self.id = self.corpo['id']
 			self.nome = self.corpo['nome']
-      self.codigo = self.codigo['codigo']
+                        self.codigo = self.codigo['codigo']
 		except:
 			raise ErroNoHTTP(400)
 		
@@ -20,9 +20,9 @@ class PedidoEditar(Pedido):
 
 	def getNome(self):
 		return self.nome
-  
-  def setCodigo(self,codigo):
-    self.codigo = codigo
-    
-  def getCodigo(self):
-    return self.codigo
+	
+        def setCodigo(self,codigo):
+                self.codigo = codigo
+		
+	def getCodigo(self):
+                return self.codigo
