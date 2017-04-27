8a81f8512bda8a5467b64dc4b4671ad50065756d
henrique1998 <henri.rnp@gmail.com>
2017-04-27 01:28:24 -0300

    Update Curso.py

--- backend/Database/Models/Curso.py
+++ backend/Database/Models/Curso.py
@@ -9,14 +9,14 @@ class Curso(object):
 			self.nome = dados ['nome']
 			self.codigo = dados ['codigo']
 			self.id_campus = dados ['id_campus']
-		  self.id_grau = dados ['id_grau']
-      self.permanencia_minima = dados ['permanencia_minima']
-      self.permanencia_maxima = dados ['permanencia_maxima']
-      self.creditos_formatura = dados ['creditos_formatura']
-      self.creditos_optativos_concentracao = dados ['creditos_optativos_concentracao']
-      self.creditos_optativos_conexa = dados ['creditos_optativos_conexa']
-      self.creditos_livres_maximo = dados ['creditos_livres_maximo']
-      
+		        self.id_grau = dados ['id_grau']
+                        self.permanencia_minima = dados ['permanencia_minima']
+                        self.permanencia_maxima = dados ['permanencia_maxima']
+                        self.creditos_formatura = dados ['creditos_formatura']
+                        self.creditos_optativos_concentracao = dados ['creditos_optativos_concentracao']
+                        self.creditos_optativos_conexa = dados ['creditos_optativos_conexa']
+                        self.creditos_livres_maximo = dados ['creditos_livres_maximo']
+	
 	def getId(self):
 		return self.id
 
@@ -40,8 +40,8 @@ class Curso(object):
 		
 	def getCampus(self):
 		return (Campus().pegarCampus('id = %s',(self.id_campus))).getNome()
-    
-  def setId_grau(self,grau):
+	
+	def setId_grau(self,grau):
 		self.id_grau = (Grau().pegarGrau('nome = %s',(grau))).getId()
 		
 	def getId_grau(self):
@@ -49,39 +49,39 @@ class Curso(object):
 		
 	def getGrau(self):
 		return (Grau().pegarGrau('id = %s',(self.id_grau))).getNome()
-    
-  def setPermanencia_minima(self,permanencia_minima):
+	
+	def setPermanencia_minima(self,permanencia_minima):
 		self.permanencia_minima = permanencia_minima
 
 	def getPermanencia_minima(self):
 		return self.permanencia_minima
-    
-  def setPermanencia_maxima(self,permanencia_maxima):
+	
+	def setPermanencia_maxima(self,permanencia_maxima):
 		self.permanencia_maxima = permanencia_maxima
-    
-  def getPermanencia_maxima(self):
+	
+	def getPermanencia_maxima(self):
 		return self.permanencia_maxima
-    
-  def setCreditos_formatura(self,creditos_formatura):
+	
+	def setCreditos_formatura(self,creditos_formatura):
 		self.creditos_formatura = creditos_formatura
-    
-  def getCreditos_formatura(self):
+	
+	def getCreditos_formatura(self):
 		return self.creditos_formatura
 
-  def setCreditos_optativos_concentracao(self,creditos_optativos_concentracao):
+        def setCreditos_optativos_concentracao(self,creditos_optativos_concentracao):
 		self.creditos_optativos_concentracao = creditos_optativos_concentracao
-    
-  def getCreditos_optativos_concentracao(self):
+	
+	def getCreditos_optativos_concentracao(self):
 		return self.creditos_optativos_concentracao
-    
-  def setCreditos_optativos_conexa(self,creditos_optativos_conexa):
+	
+	def setCreditos_optativos_conexa(self,creditos_optativos_conexa):
 		self.creditos_optativos_conexa = creditos_optativos_conexa
-    
-  def getCreditos_optativos_conexa(self):
+	
+	def getCreditos_optativos_conexa(self):
 		return self.creditos_optativos_conexa
-    
-  def setCreditos_livres_maximo(self,creditos_livres_maximo):
+	
+	def setCreditos_livres_maximo(self,creditos_livres_maximo):
 		self.creditos_livres_maximo = creditos_livres_maximo
-    
-  def getCreditos_livres_maximo(self):
+	
+	def getCreditos_livres_maximo(self):
 		return self.creditos_livres_maximo
