9700b74dc904b6287d5d2aabc341b1e22f8555f2
henrique1998 <henri.rnp@gmail.com>
2017-04-27 10:48:03 -0300

    Update PedidoEditar.py

--- backend/Models/Predio/PedidoEditar.py
+++ backend/Models/Predio/PedidoEditar.py
@@ -6,15 +6,12 @@ class PedidoEditar(Pedido):
 	def __init__(self,variaveis_do_ambiente):
 		super(PedidoEditar, self).__init__(variaveis_do_ambiente)
 		try:
-			self.id = self.corpo['id']
 			self.nome = self.corpo['nome']
                         self.sigla = self.corpo['sigla']
 			self.latitude = self.corpo['latitude']
 			self.longitude = self.corpo['longitude']
 		except:
 			raise ErroNoHTTP(400)
-	def getId(self):
-		return self.id
 		
 	def getNome(self):
 		return self.nome
