c1c903cb8d83f57ba064f3a7d6961b5449aa2d6b
henrique1998 <henri.rnp@gmail.com>
2017-04-27 10:15:38 -0300

    Update PedidoCadastrar.py

--- backend/Models/Predio/PedidoCadastrar.py
+++ backend/Models/Predio/PedidoCadastrar.py
@@ -6,37 +6,21 @@ class PedidoCadastrar(Pedido):
 	def __init__(self,variaveis_do_ambiente):
 		super(PedidoCadastrar, self).__init__(variaveis_do_ambiente)
 		try:
-			self.id = self.corpo['id']
 			self.nome = self.corpo['nome']
 			self.sigla = self.corpo['sigla']
 			self.latitude = self.corpo['latitude']
 			self.longitude = self.corpo['longitude']
 		except:
 			raise ErroNoHTTP(400)
-		
-	def getId(self):
-		return self.id
-
-	def setNome(self,nome):
-		self.nome = nome
 
 	def getNome(self):
 		return self.nome
-
-	def setSigla(self,sigla):
-		self.sigla = sigla
 		
 	def getSigla(self):
 		return self.sigla
 		
-	def setLatitude(self,latitude):
-		self.latitude = latitude
-	
 	def getLatitude(self):
 		return self.latitude
-		
-	def setLongitude(self,longitude):
-		self.longitude = longitude
 	
 	def getLongitude(self):
 		return self.longitude
