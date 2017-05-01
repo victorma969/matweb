375eef9fb264fc43fdb96ad606fa848c4d0d5483
henrique1998 <henri.rnp@gmail.com>
2017-04-30 21:38:23 -0300

    Create Fluxo.py

---
+++ backend/Database/Controllers/Fluxo.py
@@ -0,0 +1,26 @@
+from Framework.BancoDeDados import BancoDeDados
+from Database.Models.Fluxo import Fluxo as ModelFluxo
+
+
+class Fluxo(object):
+	
+	def pegarFluxo(self, condicao, valores):
+		fluxos = []
+		for fluxo in BancoDeDados().consultarMultiplos("SELECT * FROM fluxo %s" % (condicao), valores):
+			fluxos.append(ModelFluxo(fluxo))
+		return fluxos
+	
+	def pegarFluxo(self, condicao, valores):
+		return ModelFluxo(BancoDeDados().consultarUnico("SELECT * FROM fluxo %s" % (condicao), valores))
+	
+	def inserirFluxo(self, fluxo):
+		BancoDeDados().executar("INSERT INTO fluxo (periodo_inicio,periodo_fim,id_curso,id_opcao) VALUES (%s,%s,%s,%s) RETURNING id", (fluxo.periodo_inicio,fluxo.periodo_fim,fluxo.id_curso,fluxo.id_opcao))
+		fluxo.id = BancoDeDados().pegarUltimoIDInserido()
+		return fluxo
+		
+	def removerFluxo(self, fluxo):
+		BancoDeDados().executar("DELETE FROM fluxo WHERE id = %s", (str(fluxo.id)))
+		
+	def alterarFluxo(self, fluxo):
+		SQL = "UPDATE fluxo SET periodo_inicio = %s, periodo_fim = %s, id_curso = %s, id_opcao = %s WHERE id = %s"
+		BancoDeDados().executar(SQL, (fluxo.periodo_inicio,fluxo.periodo_fim,fluxo.id_curso,fluxo.id_opcao,fluxo.id))
