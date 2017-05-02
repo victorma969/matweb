4e3bb4f9b566d272d77e6b7acede0de729ddad3e
romuloalcantara <romuloos7899@gmail.com>
2017-05-02 11:20:10 -0300

    lol

--- frontend/app/Campus/campus.component.js
+++ frontend/app/Campus/campus.component.js
@@ -2,13 +2,13 @@ angular.
   module('Campus').
   component('ofertaCampus', {
     templateUrl: '/app/Campus/campus.template.html',
-    controller: ['ApiCampus', 'MatWebGlobals',function Entrar(ApiCampus,MatWebGlobals) {
+    controller: ['ApiOferta', 'MatWebGlobals',function Entrar(ApiOferta,MatWebGlobals) {
       this.nome_campus = "";
   var ctrl = this;
   ctrl.campus = [];
       this.pesquisar = function()
-      { 
-        ApiCampus.Listar({id_campus: 04 nome: ctrl.nome_campus, pagina: 0, quantidade: 1000},function(resultado) {
+      {
+        ApiOferta.Listar({id_campus: 95 , nome: ctrl.nome_campus, pagina: 0, quantidade: 1000 },function(resultado) {
               ctrl.campus = resultado.corpo
       console.log(ctrl.campus)
     }, function(erro){
