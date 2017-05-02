c159501442f9ddcba0a7b800f01d4f6e15820c87
romuloalcantara <romuloos7899@gmail.com>
2017-05-02 10:42:40 -0300

    teste

--- frontend/app/Campus/campus.service.js
+++ frontend/app/Campus/campus.service.js
@@ -2,7 +2,7 @@ angular.
   module('Campus').
   factory('ApiCampus', ['$resource',
     function($resource) {
-      return $resource('/api/Campus', {}, {});
+      return $resource('/api/Campus/Listar', {}, {});
               Listar: { method: 'POST' },
   ]);
     }
