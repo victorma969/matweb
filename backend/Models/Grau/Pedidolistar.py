06290c7ddd164ab508207713e5dff304a1907986
lucasjoseaa <lucasjoseaa@gmail.com>
2017-05-04 10:29:17 -0300

    Create Pedidolistar.py

---
+++ backend/Models/Grau/Pedidolistar.py
@@ -0,0 +1,16 @@
+from Framework.Pedido import Pedido
+from Framework.ErroNoHTTP import ErroNoHTTP
+
+class PedidoListar(Pedido):
+
+	def __init__(self,variaveis_do_ambiente):
+		super(PedidoListar, self).__init__(variaveis_do_ambiente)
+		try:
+			
+			self.nome = self.corpo['nome']
+			
+		except:
+			raise ErroNoHTTP(400)
+
+	def getNome(self):
+		return self.nome
