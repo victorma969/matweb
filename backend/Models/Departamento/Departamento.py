724ed293570799af5fb8bf89d746b7ea3df4e49a
henrique1998 <henri.rnp@gmail.com>
2017-05-04 10:59:48 -0300

    Update Departamento.py

--- backend/Models/Departamento/Departamento.py
+++ backend/Models/Departamento/Departamento.py
@@ -2,4 +2,4 @@ class Departamento(object):
 	def __init__(self,departamento):
 		self.id = departamento.getId()
 		self.nome = departamento.getNome()
-		self.id_campus = dpartamento.getId_campus()
+		self.id_campus = departamento.getId_campus()
