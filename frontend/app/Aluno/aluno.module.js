89b9f4572f961d03197fccf11d55eed5d469f9b7
Lucas Martins Alves <lalves@lucas-note.lumal21.net.br>
2017-05-09 11:31:44 -0300

    Boa Sorte

---
+++ frontend/app/Aluno/aluno.module.js
@@ -0,0 +1,3 @@
+angular.module('Aluno', [
+	'ngResource',
+	]);
---
+++ frontend/app/Aluno/aluno.service.js
@@ -0,0 +1,9 @@
+angular.
+  module('Aluno').
+  factory('ApiAluno', ['$resource',
+    function($resource) {
+      return $resource('/api/Usuario/:operacao', {}, {
+        Cadastrar: { method: 'POST', params: {'operacao' : "Cadastrar"} },
+      });
+    }
+  ]);
