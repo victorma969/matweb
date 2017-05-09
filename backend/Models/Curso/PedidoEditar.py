daa005ca47ce836fd76ce1baf850b442c9905127
henrique1998 <henri.rnp@gmail.com>
2017-05-09 09:57:10 -0300

    Update PedidoEditar.py

--- backend/Models/Curso/PedidoEditar.py
+++ backend/Models/Curso/PedidoEditar.py
@@ -10,11 +10,14 @@ class PedidoEditar(Pedido):
 			self.codigo = self.corpo['codigo']
 			self.id_grau = self.corpo['id_grau']
 			self.id_campus = self.corpo['id_campus']
+			self.permanencia_minima = self.corpo['permanencia_minima']
+			self.permanencia_maxima = self.corpo['permanencia_maxima']
+			self.creditos_formatura = self.corpo['creditos_formatura']
+			self.creditos_optativos_conexa = self.corpo['creditos_optativos_conexa']
+			self.creditos_optativos_concentracao = self.corpo['creditos_optativos_concentracao']
+			self.creditos_livres_maximo = self.corpo['creditos_livres_maximo']
 		except:
 			raise ErroNoHTTP(400)
-		
-	def getId(self):
-		return self.id
 
 	
 	def getNome(self):
