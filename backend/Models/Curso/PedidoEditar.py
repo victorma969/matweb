113813a489d6f09bfdeac5878ede9ca365578e11
henrique1998 <henri.rnp@gmail.com>
2017-05-09 03:54:46 -0300

    Create PedidoEditar.py

---
+++ backend/Models/Curso/PedidoEditar.py
@@ -0,0 +1,34 @@
+from Framework.Pedido import Pedido
+from Framework.ErroNoHTTP import ErroNoHTTP
+
+class PedidoEditar(Pedido):
+
+	def __init__(self,variaveis_do_ambiente):
+		super(PedidoEditar, self).__init__(variaveis_do_ambiente)
+		try:
+			self.nome = self.corpo['nome']
+			self.codigo = self.corpo['codigo']
+			self.id_grau = self.corpo['id_grau']
+			self.id_campus = self.corpo['id_campus']
+		except:
+			raise ErroNoHTTP(400)
+		
+	def getId(self):
+		return self.id
+
+	
+	def getNome(self):
+		return self.nome
+
+	
+	def getCodigo(self):
+		return self.codigo
+
+		
+	def getId_grau(self):
+		return self.id_grau
+		
+		
+	def getId_campus(self):
+		return self.id_campus
+		
