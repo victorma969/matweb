abaefffee4d253e98d2e93fc5816760cfe2ef58e
CaioMeloSilva <caio.cmsilva@gmail.com>
2017-05-09 11:48:33 -0300

    lol

--- frontend/Pedro Tests/controllerregistro.js
+++ frontend/Pedro Tests/controllerregistro.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app')
+        .module('Cadastro')
         .controller('RegisterController', RegisterController);
 
     RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
