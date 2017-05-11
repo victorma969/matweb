667d41a5213e3b00981a9f2bb3279b17d19ebc24
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-11 10:54:19 -0300

    lol

--- frontend/Pedro Tests/controllerregistro.js
+++ frontend/Pedro Tests/controllerregistro.js
@@ -1,29 +1,18 @@
-﻿(function () {
-    'use strict';
-
-    angular
-        .module('Cadastro')
-        .controller('RegisterController', RegisterController);
-
-    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
-    function RegisterController(UserService, $location, $rootScope, FlashService) {
-        var vm = this;
-
-        vm.register = register;
-
-        function register() {
-            vm.dataLoading = true;
-            UserService.Create(vm.user)
-                .then(function (response) {
-                    if (response.success) {
-                        FlashService.Success('Registration successful', true);
-                        $location.path('/login');
-                    } else {
-                        FlashService.Error(response.message);
-                        vm.dataLoading = false;
-                    }
-                });
-        }
-    }
-
-})();
+angular.
+  module('Sala').
+  component('salaRegistrar', {
+    templateUrl: '/app/Sala/cadastrarsala.template.html',
+    controller: ['ApiSala','$http','$location', 'MatWebGlobals',function Registrar(ApiAluno,$http,$location,MatWebGlobals) {
+      var ctrl = this;
+      this.formulario = {'predio':'','corredor':'','nomedasala':''};
+      this.cadastrar = function()
+      {
+          ApiAluno.Cadastrar(ctrl.formulario,function(data){
+            ctrl.mensagem = "Sala cadastrada com sucesso";
+          },function(data){
+            ctrl.mensagem = "ERRO";
+          });
+   		};
+   	  }
+    }]
+});
--- frontend/Pedro Tests/oferta.services.js
+++ frontend/Pedro Tests/oferta.services.js
@@ -2,11 +2,12 @@ angular.
   module('Sala').
   factory('ApiSala', ['$resource',
     function($resource) {
-      return $resource('/api/Sala/Cadastrar', {}, {
-        Cadastrar: { method: 'POST' },
+      return $resource('/api/Sala/:operacao', {}, {
+        Cadastrar: { method: 'POST', params: {'operacao' : "Cadastrar"} },
         Editar: { method: 'POST' },
         Listar: { method: 'POST' },
         Excluir: { method: 'POST' },
       });
     }
   ]);
+
