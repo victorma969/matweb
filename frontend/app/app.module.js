3775775ff343b346c2e88cdf9969030d2bc3c69d
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-05-13 20:37:36 -0300

    Erro de sitaxe no modulo

--- frontend/app/app.module.js
+++ frontend/app/app.module.js
@@ -4,10 +4,7 @@ angular.module('MatWeb', [
   'Usuario',
   'Oferta',
   'Campus',
-<<<<<<< HEAD
   'Curso',
-=======
   'Registrodesala',
->>>>>>> 9e8df66f4ce6cb75944d9132f5acb08849c58789
   'ui.bootstrap',
 ]);
