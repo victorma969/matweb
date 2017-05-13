fac03757a6f20dafaaeb41cdffed2613f61e772f
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-13 01:41:41 -0300

    lol

--- frontend/app/nucleo/tela-principal.component.js
+++ frontend/app/nucleo/tela-principal.component.js
@@ -1,7 +1,7 @@
 angular.
   module('nucleo').
   component('telaPrincipal', {
-    templateUrl: "https://pedrogabriel96.matweb.lumal21.net.br/OLD/MatriculaWEB/index.html",
+    templateUrl: '/app/nucleo/tela-principal.template.html',
     controller: ['MatWebGlobals', function Menu(MatWebGlobals) {
       
     }]
