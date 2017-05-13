96f0b958b0c9a9c8bf30a9cf43b35dbf312b8c1e
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-12 23:11:14 -0300

    lol

--- frontend/app/Registrodesala/registrosala.services.js
+++ frontend/app/Registrodesala/registrosala.services.js
@@ -2,7 +2,7 @@ angular.
   module('Registrodesala').
   factory('ApiSala', ['$resource',
     function($resource) {
-      return $resource('/api/Sala/Cadastrar/:operacao', {}, {
+      return $resource('/api/Sala/:operacao', {}, {
         Cadastrar: { method: 'POST', params: {'operacao' : "Cadastrar"} },
         Editar: { method: 'POST' },
         Listar: { method: 'POST' },
