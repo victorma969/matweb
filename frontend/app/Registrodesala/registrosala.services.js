9109bfb1319107249beb5feac62f6e6ce35923d1
CaioMeloSilva <caio.cmsilva@gmail.com>
2017-05-16 11:34:31 -0300

    lol

--- frontend/app/Registrodesala/registrosala.services.js
+++ frontend/app/Registrodesala/registrosala.services.js
@@ -2,7 +2,7 @@ angular.
   module('Registrodesala').
   factory('ApiSala', ['$resource',
     function($resource) {
-      return $resource('/api/Disciplina/Listar', {}, {
+      return $resource('/api/Sala/Cadastrar', {}, {
         Cadastrar: { method: 'POST' },
       });
     }
