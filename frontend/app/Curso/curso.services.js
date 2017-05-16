25cca606458f24c5eb6d71a5fe6d2eed8fc62733
Lucas Martins Alves <contato@lumal21.net.br>
2017-05-16 11:20:39 -0300

    Update curso.services.js

--- frontend/app/Curso/curso.services.js
+++ frontend/app/Curso/curso.services.js
@@ -2,9 +2,11 @@ angular.
   module('Curso').
   factory('ApiCurso', ['$resource',
     function($resource) {
-      return $resource('/api/Curso/Listar', {}, {
+      var x = $resource('/api/Curso/Listar', {}, {
         Listar: { method: 'POST' },
 
       });
+      console.log(x);
+      return x;
     }
 ]);
