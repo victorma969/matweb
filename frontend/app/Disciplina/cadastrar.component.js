e603a91b076d66f9da6ae44d077c97527f6e567c
FelipeAires <FelipeAires>
2017-05-16 11:03:03 -0300

    gdgdfd3

--- frontend/app/Disciplina/cadastrar.component.js
+++ frontend/app/Disciplina/cadastrar.component.js
@@ -10,13 +10,16 @@ angular.
     var codigo_disciplin = "";
     this.formulario = {nome: nome_disciplin, codigo: codigo_disciplin, id_departamento: 95 }
     this.cadastrardisciplina = function()
+    console.log("Se funfo")
     {
 
       ApiDisciplina.Cadastrar(ctrl.formulario, function(data)
       {
         ctrl.mensagem = "Cadastro de disciplina realizado com sucesso !";
+        console.log("Se foi cadastrado")
       }, function(data){
         crtl.mensagem = "Erro ao cadastrar disciplina!";
+        console.log("Se n foi cadastrado")
                         }                       );
 
     }
