91086d857facd021fa5e28e848eac1b5d8d227c9
CaioMeloSilva <caio.cmsilva@gmail.com>
2017-05-16 11:53:09 -0300

    lol

--- frontend/app/Registrodesala/registrosala.component.js
+++ frontend/app/Registrodesala/registrosala.component.js
@@ -2,7 +2,7 @@ angular.
   module('Registrodesala').
   component('salaRegistrar', {
     templateUrl: '/app/Registrodesala/registrodesala.template.html',
-    controller: ['ApiSala','$http','$location', 'MatWebGlobals',function (ApiSala,$http,$location,MatWebGlobals) {
+    controller: ['ApiSala','$http','$location', 'MatWebGlobals',function(ApiSala,$http,$location,MatWebGlobals) {
       var ctrl = this;
       this.formulario = {'predioName':'','corredorNumber':'','salaname':''};
       this.cadastrar = function()
