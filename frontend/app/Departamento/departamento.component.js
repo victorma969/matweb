95b8715dcdf6aa9e8acc9f1e5055056f170d1005
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-17 18:58:50 -0300

    lol 2017-05-17

--- frontend/app/Departamento/departamento.component.js
+++ frontend/app/Departamento/departamento.component.js
@@ -8,7 +8,7 @@ angular.
 	ctrl.departamento = [];
       this.pesquisar = function()
       {
-       	ApiDepartamento.Listar({id_campus: 95 , nome: ctrl.nome_departamento, pagina: 0, quantidade: 1000 },function(resultado) {
+       	ApiDepartamento.Listar({id_campus: 1 , nome: ctrl.nome_departamento, pagina: 0, quantidade: 1000 },function(resultado) {
 		          ctrl.departamento = resultado.corpo
 			console.log(ctrl.departamento)
 		}, function(erro){
