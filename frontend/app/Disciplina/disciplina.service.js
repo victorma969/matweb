10cc1168adc1efe58124e4524b0ec07ae7212452
FelipeAires <felipeaires2008@gmail.com>
2017-05-17 18:38:57 -0300

    tentando funfar as paradas de oferta3

--- frontend/app/Disciplina/disciplina.service.js
+++ frontend/app/Disciplina/disciplina.service.js
@@ -3,9 +3,8 @@ angular.
   factory('ApiDisciplina', ['$resource',
     function($resource) {
       return $resource('/api/Disciplina/:operacao', {}, {
-      	Listar: { method: 'POST', params: {'operacao: "Listar"}}
+      	Listar: { method: 'POST', params: {'operacao': "Listar"}}
         Cadastrar: { method: 'POST', params: {'operacao': "Cadastrar"} }
       });
     }
   ]);
-  
