0556aa088936b5fa1e3a783bbd05f4725808beee
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-17 22:15:52 -0300

    lol 2017-05-17

--- frontend/app/Cadastrar/cadastrar.component.js
+++ frontend/app/Cadastrar/cadastrar.component.js
@@ -1,13 +1,13 @@
 angular.
   module('Departamento').
-  component('departamentoRegistrar', {
+  component('registrarDepartamento', {
     templateUrl: '/app/Cadastrar/cadastrar.template.html',
     controller: ['ApiDepartamento','$http','$location', 'MatWebGlobals',function(ApiDepartamento,$http,$location,MatWebGlobals) {
       var ctrl = this;
-      this.formulario = {'predioName':'','corredorNumber':''};
+      this.formulario = {'depName':'','idNumber':''};
       this.cadastrar = function()
       {
-          ApiSala.Cadastrar(ctrl.formulario,function(data){
+          ApiDepartamento.Cadastrar(ctrl.formulario,function(data){
             ctrl.mensagem = "Departamento cadastrado com sucesso";
           },function(data){
             ctrl.mensagem = "ERRO";
