0ae5faef401844fc0eedf33101e673a975349f99
CaioMeloSilva <caio.cmsilva@gmail.com>
2017-05-18 10:54:39 -0300

    lol 2017-05-18

--- frontend/app/Cadastrar/cadastrar.component.js
+++ frontend/app/Cadastrar/cadastrar.component.js
@@ -5,9 +5,8 @@ angular.
     controller: ['ApiDepartamentoRegistrar','$http','$location', 'MatWebGlobals',function(ApiDepartamentoRegistrar,$http,$location,MatWebGlobals) {
       var ctrl = this;
       this.formulario = {'depName':''};
-      this.cadastrar = function()
       {
-          ApiDepartamentoRegistrar.Cadastrar(ctrl.formulario,function(data){
+          ApiDepartamentoRegistrar.cadastrar(ctrl.formulario,function(data){
             ctrl.mensagem = "Departamento cadastrado com sucesso";
           },function(data){
             ctrl.mensagem = "ERRO";
