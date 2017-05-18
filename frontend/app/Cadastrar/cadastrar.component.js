5fb00799c8c38c16d1dec213a748d63ad0d970ce
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-17 23:19:36 -0300

    lol 2017-05-17

--- frontend/app/Cadastrar/cadastrar.component.js
+++ frontend/app/Cadastrar/cadastrar.component.js
@@ -1,23 +1,29 @@
-angular.
-  module('Cadastrar').
-  component('registrarDepartamento', {
-    templateUrl: '/app/Cadastrar/cadastrar.template.html',
-    controller: ['ApiDepartamento','$http','$location', 'MatWebGlobals',function registrarDepartamento(ApiDepartamento,$http,$location,MatWebGlobals) {
-      var ctrl = this;
-      ctrl.registrar = registrar;
-      this.formulario = {'depName':'','idNumber':''};
- function.registrar()
-  ctrl.mensagem = true;
-  ApiDepartamento.Create(ctrl.formulario)
-  .then(function (data){ 
-    if(data.success){
-MatWebGlobals.Success('registro feito com sucesso', true);
-$http.path('/Cadastrar');
-} else{
-  MatWebGlobals.Error(data.messagem);
-ctrl.mensagem = false;
-}
-});
+(function () {
+'use strict';
+angular
+        .module('Cadastrar')
+        .controller('registrarDepartamento', registrarDepartamento)
+        templateUrl: '/app/Cadastrar/cadastrar.template.html',
 
-    }]
-}
+    registrarDepartamento.$inject = ['ApiDepartamento', '$http', '$location', 'MatWebGlobals'];
+    function registrarDepartamento(ApiDepartamento, $http, $location, MatWebGlobals) {
+        var ctrl = this;
+this.formulario = {'depName':'','idNumber':''};
+        ctrl.cadastrar = cadastrar;
+
+        function cadastrar() {
+            ctrl.mensagem = true;
+            UserService.Create(ctrl.formulario)
+                .then(function (data) {
+                    if (data.success) {
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/Cadastrar');
+                    } else {
+                        FlashService.Error(data.message);
+                        ctrl.mensagem = false;
+                    }
+                });
+        }
+    }
+
+})();
