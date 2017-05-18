3f6b909720f324f2a8c78cf1990ec1a6397a271f
CaioMeloSilva <caio.cmsilva@gmail.com>
2017-05-18 10:46:24 -0300

    lol 2017-05-18

--- frontend/app/Cadastrar/cadastrar.component.js
+++ frontend/app/Cadastrar/cadastrar.component.js
@@ -1,22 +1,17 @@
 angular.
-module('Cadastrar').
+  module('Cadastrar').
   component('registrarDepartamento', {
     templateUrl: '/app/Cadastrar/cadastrar.template.html',
-    controller: ['ApiDepartamento','$http','$location', 'MatWebGlobals',function registrarDepartamento(ApiDepartamento,$http,$location,MatWebGlobals) {
-        var ctrl = this;
-        ctrl.cadastrar = cadastrar;
-        function cadastrar() {
-            ctrl.mensagem = true;
-            ApiDepartamento.Create(ctrl.formulario)
-                .then(function (data) {
-                    if (data.success) {
-                        MatWebGlobals.Success('Registration successful', true);
-                        $location.path('/Cadastrar');
-                    } else {
-                        MatWebGlobals.Error(data.mensagem);
-                        ctrl.mensagem = false;
-                    }
-                })
-          };
+    controller: ['ApiDepartamentoRegistrar','$http','$location', 'MatWebGlobals',function(ApiDepartamentoRegistrar,$http,$location,MatWebGlobals) {
+      var ctrl = this;
+      this.formulario = {'predioName':'','corredorNumber':'','salaname':''};
+      this.cadastrar = function()
+      {
+          ApiDepartamentoRegistrar.Cadastrar(ctrl.formulario,function(data){
+            ctrl.mensagem = "Departamento cadastrado com sucesso";
+          },function(data){
+            ctrl.mensagem = "ERRO";
+          });
+    };
     }]
 });
