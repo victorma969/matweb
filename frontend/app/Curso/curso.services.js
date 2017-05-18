06893f41788318328ba9529e302d0a8f3dda6899
CaioMeloSilva <caio.cmsilva@gmail.com>
2017-05-18 13:19:38 -0300

    lol

--- frontend/app/Curso/curso.services.js
+++ frontend/app/Curso/curso.services.js
@@ -2,14 +2,8 @@ angular.
   module('Curso').
   factory('ApiCurso', ['$resource',
     function($resource) {
-<<<<<<< HEAD
       return $resource('/api/Curso/Listar', {}, {
         Listar: { method: 'POST'},
-=======
-        return $resource('/api/Curso/:operacao', {}, {
-        Listar: { method: 'POST', params: {'operacao': "Listar"} }, 
-        Cadastrar: { method: 'POST', params: {'operacao': "Cadastrar"} },
->>>>>>> f80fe639810699e2156f30b85a7467289c349e30
       });
     }
   ]);
