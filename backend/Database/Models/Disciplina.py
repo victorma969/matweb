b3b22d318c85b7aa060cebc853b9e566c1ed504b
henrique1998 <henri.rnp@gmail.com>
2017-05-18 00:22:19 -0300

    Update Disciplina.py

--- backend/Database/Models/Disciplina.py
+++ backend/Database/Models/Disciplina.py
@@ -8,6 +8,7 @@ class Disciplina(object):
 			self.nome = dados ['nome']
 			self.codigo = dados ['codigo']
 			self.id_departamento = dados ['id_departamento']
+			self.b = dados ['b']
 			
 	def getId(self):
 		return self.id
@@ -32,3 +33,9 @@ class Disciplina(object):
 		
 	def getDepartamento(self):
 		return (Departamento().pegarDepartamento('id = %s',(self.id_departamento,))).getNome()
+
+	def setB(self,b):
+		self.b = b
+
+	def getB(self):
+		return self.b
