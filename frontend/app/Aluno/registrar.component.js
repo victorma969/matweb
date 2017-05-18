ba458c2d9bc06a44191a5a862272def3952f6d16
Gabriel <gabryelll.carvalho@gmail.com>
2017-05-18 10:19:14 -0300

    teste

--- frontend/app/Aluno/registrar.component.js
+++ frontend/app/Aluno/registrar.component.js
@@ -4,16 +4,8 @@ angular.
     templateUrl: '/app/Aluno/aluno.registrar.html',
     controller: ['ApiAluno','$http','$location', 'MatWebGlobals',function Registrar(ApiAluno,$http,$location,MatWebGlobals) {
       var ctrl = this;
-      var n = "";
-      var m = "":
-      var c = "":
-      var i = "":
-      var e = "":
-      var s = "":
-      var u = "":
-
-      this.formulario = {nome:n,'matricula':'','cpf':'','identidade':'','email':'','sexo':'','uf':'','cor':'','nivel':'','conclusao':'','nome_pai':'','nome_mae':'','senha':'','cep':'','complemento':'','numero_telefone':'','numero_lote':'','id_raca_cor':'','
-,'id_nivel':'','ano_conclusao':'','tipo_escola':''e};
+      this.formulario = {'nome':'','matricula':'','cpf':'','identidade':'','email':'','sexo':'','uf':'','cor':'','nivel':'','conclusao':'','nome_pai':'','nome_mae':'','senha':'','cep':'','complemento':'','numero_telefone':'','numero_lote':'','id_raca_cor':'','
+,'id_nivel':'','ano_conclusao':'','tipo_escola':''};
       this.cadastrar = function()
       {
           ApiAluno.Cadastrar(ctrl.formulario,function(data){
