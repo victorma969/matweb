18f8f1c438e19cf7badc78e686ebbabf7b2ae8d3
CaioMeloSilva <caio.cmsilva@gmail.com>
2017-05-18 13:26:55 -0300

    lol

--- frontend/app/Curso/curso.component.js
+++ frontend/app/Curso/curso.component.js
@@ -4,17 +4,17 @@ angular.
     templateUrl: '/app/Curso/curso.template.html',
     controller: ['ApiCurso', 'MatWebGlobals',function Entrar(ApiCurso,MatWebGlobals) {
       this.nome_curso = "";
-	var ctrl = this;
-	ctrl.cursos = [];
+  var ctrl = this;
+  ctrl.cursos = [];
       this.pesquisar = function()
       {
-       	ApiCurso.Listar({id_campus:1 , nome: ctrl.nome_curso, pagina: 0, quantidade: 1000 },function(resultado) {
-		          ctrl.cursos = resultado.corpo
-			console.log(ctrl.cursos)
-		}, function(erro){
-   			ctrl.erro = erro.data.mensagem
-			console.log(ctrl.erro)
-   		} );
-   	  }
+        ApiCurso.Listar({id_campus: 95 , nome: ctrl.nome_curso, pagina: 0, quantidade: 1000 },function(resultado) {
+              ctrl.cursos = resultado.corpo
+      console.log(ctrl.cursos)
+    }, function(erro){
+        ctrl.erro = erro.data.mensagem
+      console.log(ctrl.erro)
+      } );
+      }
     }]
   });
