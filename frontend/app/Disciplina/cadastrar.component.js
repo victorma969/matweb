6eadc589127fa68858e30aea70b557f75f530a8c
FelipeAires <felipeaires2008@gmail.com>
2017-05-21 17:07:59 -0300

    teste

--- frontend/app/Disciplina/cadastrar.component.js
+++ frontend/app/Disciplina/cadastrar.component.js
@@ -4,12 +4,13 @@ angular.
     templateUrl: '/app/Disciplina/cadastrar.template.html',
     controller: ['ApiDisciplina', 'MatWebGlobals',function Cadastro(ApiDisciplina,MatWebGlobals) 
 {
-  
+
     var ctrl = this;
     this.formulario = {'nome':'', 'codigo': '', 'id_departamento':''}
     this.cadastrardisciplina = function()
     {
       console.log("Se funfo")
+      console.log(cadastrardisciplina)
 
       ApiDisciplina.Cadastrar(ctrl.formulario, function(data)
       {
