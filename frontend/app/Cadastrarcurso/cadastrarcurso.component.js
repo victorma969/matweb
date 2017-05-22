5de4687b2095e6584f168294c4c76c0633507de4
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-22 09:54:50 -0300

    lol 2017-05-22

--- frontend/app/Cadastrarcurso/cadastrarcurso.component.js
+++ frontend/app/Cadastrarcurso/cadastrarcurso.component.js
@@ -4,7 +4,7 @@ angular.
     templateUrl: '/app/Cadastrarcurso/cadastrarcurso.template.html',
     controller: ['ApiRegistroCurso','$http','$location', 'MatWebGlobals',function(ApiRegistroCurso,$http,$location,MatWebGlobals) {
       var ctrl = this;
-      this.formulario = {'nome':'','id_campus':'','id_grau':'','permanencia_minima':'','permanencia_maxima':'','creditos_formatura':'','creditos_optativos_concentracao':'','creditos_optativos_conexa':'','creditos_livres_maximo':'''codigo':'0',};
+      this.formulario = {'nome':'','id_campus':'','id_grau':'','permanencia_minima':'','permanencia_maxima':'','creditos_formatura':'','creditos_optativos_concentracao':'','creditos_optativos_conexa':'','creditos_livres_maximo':'','codigo':'0',};
       this.cadastrar = function()
       {
           ApiRegistroCurso.Cadastrar(ctrl.formulario,function(data){
--- frontend/app/Curso/curso.component.js
+++ frontend/app/Curso/curso.component.js
@@ -8,13 +8,13 @@ angular.
 	ctrl.cursos = [];
       this.pesquisar = function()
       {
-       	ApiCurso.Listar({id_campus: 1, id_grau: 1, nome: ctrl.nome_curso, pagina: 0, quantidade: 1000 },function(resultado) {
+       	ApiCurso.Listar({nome: ctrl.nome_curso, id_campus: 1, id_grau: 1, pagina: 0, quantidade: 1000 },function(resultado) {
 		          ctrl.cursos = resultado.corpo
-			console.log(ctrl.cursos)
-		}, function(erro){
-   			ctrl.erro = erro.data.mensagem
-			console.log(ctrl.erro)
-   		} );
-   	  }
+      console.log(ctrl.cursos)
+    }, function(erro){
+        ctrl.erro = erro.data.mensagem
+      console.log(ctrl.erro)
+      } );
+      }
     }]
   });
