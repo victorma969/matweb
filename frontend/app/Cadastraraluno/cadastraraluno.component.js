c7de494c7b97d124e8104615981b8716c84d1d8a
Gabriel <gabryelll.carvalho@gmail.com>
2017-05-23 11:08:02 -0300

    teste

--- frontend/app/Cadastraraluno/cadastraraluno.component.js
+++ frontend/app/Cadastraraluno/cadastraraluno.component.js
@@ -4,7 +4,7 @@ angular.
     templateUrl: '/app/Cadastraraluno/cadastraraluno.template.html',
     controller: ['ApiUsuarioCadastrar','$http','$location', 'MatWebGlobals',function(ApiUsuarioCadastrar,$http,$location,MatWebGlobals) {
       var ctrl = this;
-      this.formulario = {'matricula':'','senha':'','email':''};
+      this.formulario = {'matricula':'','senha':'','email':'','nome':''};
       this.cadastrar = function()
       {
           ApiUsuarioCadastrar.Cadastrar(ctrl.formulario,function(data){
