dc91f7ecbc67c9bb8f7bf4b1f0815f544a348142
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-22 21:55:23 -0300

    lol 2017-05-22

--- frontend/app/Cadastrarcurso/cadastrarcurso.component.js
+++ frontend/app/Cadastrarcurso/cadastrarcurso.component.js
@@ -4,7 +4,7 @@ angular.
     templateUrl: '/app/Cadastrarcurso/cadastrarcurso.template.html',
     controller: ['ApiRegistroCurso','$http','$location', 'MatWebGlobals',function(ApiRegistroCurso,$http,$location,MatWebGlobals) {
       var ctrl = this;
-      this.formulario = {'nome':'','mec':'sim','id_campus':'','id_grau':'','permanencia_minima':'','permanencia_maxima':'','creditos_formatura':'','creditos_optativos_concentracao':'','creditos_optativos_conexa':'','creditos_livres_maximo':'','codigo':'',};
+      this.formulario = {'nome':'','mec':'','id_campus':'','id_grau':'','permanencia_minima':'','permanencia_maxima':'','creditos_formatura':'','creditos_optativos_concentracao':'','creditos_optativos_conexa':'','creditos_livres_maximo':'','codigo':'',};
       this.cadastrar = function()
       {
           ApiRegistroCurso.Cadastrar(ctrl.formulario,function(data){
