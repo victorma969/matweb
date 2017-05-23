f822da442e3a188dd6f9d53679734837bfcb7d9e
Gabrielunb <gabryelll.carvalho@gmail.com>
2017-05-22 22:39:00 -0300

    teste

--- frontend/app/Usuario/registrar.component.js
+++ frontend/app/Usuario/registrar.component.js
@@ -3,11 +3,10 @@ angular.
   component('usuarioRegistrar', {
     templateUrl: '/app/Usuario/registrar.template.html',
     controller: ['ApiUsuario','$http','$location', 'MatWebGlobals',function Registrar(ApiUsuario,$http,$location,MatWebGlobals) {
-      this.formulario = {'nome':'','matricula':'','cpf':'','identidade':'','email':'','sexo':'','uf':'','conclusao':'','nome_pai':'','nome_mae':'','senha':'','cep':'','complemento':'','numero_telefone':'','numero_lote':'','id_raca_cor':'','
-,'ano_conclusao':'','tipo_escola':''};
-      this.cadastrar = function()
+      this.formulario = {'nome':'','matricula':'','cpf':'','identidade':'','email':'','sexo':'','uf':'','conclusao':'','nome_pai':'','nome_mae':'','senha':'','cep':'','complemento':'','numero_telefone':'','numero_lote':'','ano_conclusao':'','tipo_escola':''};
+      this.registrar = function()
       {
-          ApiAluno.Cadastrar(ctrl.formulario,function(data){
+          ApiUsuario.Cadastrar(ctrl.formulario,function(data){
             ctrl.mensagem = "Cadastrado com sucesso";
           },function(data){
             ctrl.mensagem = "ERRO";
