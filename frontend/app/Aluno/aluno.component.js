255179e73b91f47aec71cdbfd2c98cf619f17757
Gabrielunb <gabryelll.carvalho@gmail.com>
2017-05-22 23:24:07 -0300

    teste

--- frontend/app/Aluno/aluno.component.js
+++ frontend/app/Aluno/aluno.component.js
@@ -2,7 +2,7 @@ angular.
   module('Aluno').
   component('alunoRegistrar', {
     templateUrl: '/app/Aluno/registrar.template.html',
-    controller: ['ApiAluno','$http','$location', 'MatWebGlobals',function(ApiAluno,$http,$location,MatWebGlobals) {
+    controller: ['ApiAluno','$http','$location', 'MatWebGlobals',function Registrar (ApiAluno,$http,$location,MatWebGlobals) {
       this.formulario = {'nome':'','matricula':'','cpf':'','identidade':'','email':'','sexo':'','uf':'','conclusao':'','nome_pai':'','nome_mae':'','senha':'','cep':'','complemento':'','numero_telefone':'','numero_lote':'','ano_conclusao':'','tipo_escola':''};
       this.registrar = function()
       {
