a5cc6392ccd7901622fd076908175083f4dda367
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-23 00:30:03 -0300

    lol 2017-05-23

--- frontend/app/Home/home.component.js
+++ frontend/app/Home/home.component.js
@@ -2,8 +2,4 @@ angular.
   module('Home').
   component('casaUsuario', {
     templateUrl: '/app/Home/home.template.html',
-    controller: ['ApiHome', 'MatWebGlobals',function Entrar(ApiHome,MatWebGlobals) {
-      this.nome_usuario = "";
-	var ctrl = this;
-	    }]
   });
