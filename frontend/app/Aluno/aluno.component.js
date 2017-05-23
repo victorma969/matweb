140dbe1944c99b05f9edb6faca7800545429da77
Gabrielunb <gabryelll.carvalho@gmail.com>
2017-05-22 23:22:01 -0300

    teste

--- frontend/app/Aluno/aluno.component.js
+++ frontend/app/Aluno/aluno.component.js
@@ -2,11 +2,11 @@ angular.
   module('Aluno').
   component('alunoRegistrar', {
     templateUrl: '/app/Aluno/registrar.template.html',
-    controller: ['ApiAluno','$http','$location', 'MatWebGlobals',function Registrar(ApiAluno,$http,$location,MatWebGlobals) {
+    controller: ['ApiAluno','$http','$location', 'MatWebGlobals',function(ApiAluno,$http,$location,MatWebGlobals) {
       this.formulario = {'nome':'','matricula':'','cpf':'','identidade':'','email':'','sexo':'','uf':'','conclusao':'','nome_pai':'','nome_mae':'','senha':'','cep':'','complemento':'','numero_telefone':'','numero_lote':'','ano_conclusao':'','tipo_escola':''};
       this.registrar = function()
       {
-          ApiAluno.Registrar(this.ctrl.formulario,function(data){
+          ApiAluno.Cadastrar(this.ctrl.formulario,function(data){
             ctrl.mensagem = "Cadastrado com sucesso";
           },function(data){
             ctrl.mensagem = "ERRO";
