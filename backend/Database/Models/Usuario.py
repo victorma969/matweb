50c6e3397503c826d7c75308d838471f894a876b
Gabrielunb <gabryelll.carvalho@gmail.com>
2017-05-22 23:57:30 -0300

    teste

--- backend/Database/Models/Usuario.py
+++ backend/Database/Models/Usuario.py
@@ -12,14 +12,14 @@ class Usuario(object):
 			self.sexo = dados['sexo']
 			self.nome_pai = dados['nome_pai']
 			self.nome_mae = dados['nome_mae']
-			self.id_raca_cor = dados['id_raca_cor']
-			self.id_nivel = dados['id_nivel']
+		#	self.id_raca_cor = dados['id_raca_cor']
+		#	self.id_nivel = dados['id_nivel']
 			self.ano_conclusao = dados['ano_conclusao']
-			self.cep = dados['cep']
-			self.numero_lote = dados['numero_lote']
-			self.complemento = dados['complemento']
-			self.numero_telefone = dados['numero_telefone']
-			self.tipo_escola = dados['tipo_escola']
+		#	self.cep = dados['cep']
+		#	self.numero_lote = dados['numero_lote']
+		#	self.complemento = dados['complemento']
+		#	self.numero_telefone = dados['numero_telefone']
+		#	self.tipo_escola = dados['tipo_escola']
 
 	def getId(self):
 		return self.id
@@ -78,17 +78,17 @@ class Usuario(object):
 	def setNome_mae(self,nome_mae):
 		self.nome_mae = nome_mae
 
-	def getId_raca_cor(self):
-		return self.raca_cor	
+#	def getId_raca_cor(self):
+#		return self.raca_cor	
 
-	def setId_raca_cor(self,id_raca_cor):
-		self.id_raca_cor = id_raca_cor
+#	def setId_raca_cor(self,id_raca_cor):
+#		self.id_raca_cor = id_raca_cor
 	
-	def getId_nivel(self):
-		return self.nivel
+#	def getId_nivel(self):
+#		return self.nivel
 
-	def setNivel(self,nivel):
-		self.nivel = nivel
+#	def setNivel(self,nivel):
+#		self.nivel = nivel
 
 	def getAno_conclusao(self):
 		return self.ano_conclusao
@@ -96,29 +96,29 @@ class Usuario(object):
 	def setAno_conclusao(self,ano_conclusao):
 		self.ano_conclusao = ano_conclusao
 
-	def getCep(self):
-		return self.cep
+#	def getCep(self):
+#		return self.cep
 
-	def setCep(self,cep):
-		self.cep = cep
+#	def setCep(self,cep):
+#		self.cep = cep
 
-	def getNumero_lote(self):
-		return self.numero_lote
+#	def getNumero_lote(self):
+#		return self.numero_lote
 
-	def setNumero_lote(self,numero_lote):
-		self.numero_lote = numero_lote
+#	def setNumero_lote(self,numero_lote):
+#		self.numero_lote = numero_lote
 
-	def getComplemento(self):
-		return self.complemento
+#	def getComplemento(self):
+#		return self.complemento
 
-	def setComplemento(self,complemento):
-		self.complemento = complemento
+#	def setComplemento(self,complemento):
+#		self.complemento = complemento
 	
-	def getNumero_telefone(self):
-		return self.numero_telefone
+#	def getNumero_telefone(self):
+#		return self.numero_telefone
 
-	def setNumero_telefone(self,numero_telefone):
-		self.numero_telefone = numero_telefone
+#	def setNumero_telefone(self,numero_telefone):
+#		self.numero_telefone = numero_telefone
 	
 	def getAno_conclusao(self):
 		return self.ano_conclusao
@@ -126,8 +126,8 @@ class Usuario(object):
 	def setAno_conclusao(self,ano_conclusao):
 		self.ano_conclusao = ano_conclusao
 
-	def getTipo_escola(self):
-		return self.tipo_escola
+#	def getTipo_escola(self):
+#		return self.tipo_escola
 
-	def setTipo_escola(self,tipo_escola):
-		self.tipo_escola = tipo_escola
+#	def setTipo_escola(self,tipo_escola):
+#		self.tipo_escola = tipo_escola
--- frontend/app/Aluno/aluno.component.js
+++ frontend/app/Aluno/aluno.component.js
@@ -3,7 +3,7 @@ angular.
   component('alunoRegistrar', {
     templateUrl: '/app/Aluno/registrar.template.html',
     controller: ['ApiAluno','$http','$location', 'MatWebGlobals',function Registrar (ApiAluno,$http,$location,MatWebGlobals) {
-      this.formulario = {'nome':'','matricula':'','cpf':'','identidade':'','email':'','sexo':'','uf':'','conclusao':'','nome_pai':'','nome_mae':'','senha':'','cep':'','complemento':'','numero_telefone':'','numero_lote':'','ano_conclusao':'','tipo_escola':''};
+      this.formulario = {'nome':'','matricula':'','cpf':'','identidade':'','email':'','sexo':'','uf':'','conclusao':'','nome_pai':'','nome_mae':'','senha':'','complemento':'','ano_conclusao':''};
       this.registrar = function()
       {
           ApiAluno.Cadastrar(ctrl.formulario,function(data){
