2b41345569ac6578feaae15b89c095c9e5e2b52d
master <pedro.gabriel.nunes@gmail.com>
2017-05-25 12:24:57 -0300

    lol 2017-05-25

--- frontend/app/Usuario/entrar.component.js
+++ frontend/app/Usuario/entrar.component.js
@@ -11,9 +11,9 @@ angular.
        		$http.defaults.headers.common.Authorization = resultado.corpo.token;
        		window.localStorage.setItem('token_de_acesso', resultado.corpo.token);
        		$location.path('/Usuario/Home')
-   		},,function(resultado){
+   		},function(resultado){
             ctrl.mensagem = "ERRO";
-          }
+          });
    	  }
     }]
   });
