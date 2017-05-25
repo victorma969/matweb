0ed6ff7ca51a85a1b3146a7362fe60617010e0b5
henrique1998 <henri.rnp@gmail.com>
2017-05-25 11:58:57 -0300

    Update Pedido_Cadastrar.py

--- backend/Models/Turma/Pedido_Cadastrar.py
+++ backend/Models/Turma/Pedido_Cadastrar.py
@@ -5,8 +5,8 @@ class PedidoCadastrar(Pedido):
 
 	def __init__(self,variaveis_do_ambiente):
 		super(PedidoCadastrar, self).__init__(variaveis_do_ambiente)
-    try:    
-    
+		try:    
+	
 			self.letra = self.corpo['letra']
 			self.id_disciplina = self.corpo['id_dsciplina']
 		except:
