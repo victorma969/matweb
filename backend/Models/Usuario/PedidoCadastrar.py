a5c638f046f163a31c23b57032cb36c99598b06c
Gabrielunb <gabryelll.carvalho@gmail.com>
2017-05-28 19:01:42 -0300

    Cadastro

--- backend/Models/Usuario/PedidoCadastrar.py
+++ backend/Models/Usuario/PedidoCadastrar.py
@@ -10,6 +10,12 @@ class PedidoCadastrar(Pedido):
 			self.matricula = self.corpo['matricula']
 			self.cpf = self.corpo['cpf']
 			self.perfil = self.corpo['perfil']
+			self.email = self.corpo['email']
+			self.sexo = self.corpo['sexo']
+			self.nome_pai = self.corpo['nome_pai']
+			self.nome_mae = self.corpo['nome_mae']
+			self.ano_conclusao = self.corpo['ano_conclusao']
+			self.identidade = self.corpo['identidade']
 			self.senha = self.corpo['senha']
 		except:
 			raise ErroNoHTTP(400)
@@ -28,3 +34,21 @@ class PedidoCadastrar(Pedido):
 
 	def getSenha(self):
 		return self.senha
+
+	def getEmail(self):
+		return self.email
+
+	def getSexo(self):
+		return self.sexo
+
+	def getNome_pai(self):
+		return self.nome_pai		
+
+	def getNome_mae(self):
+		return self.nome_mae
+
+	def getAno_conclusao(self):
+		return self.ano_conclusao
+
+	def getIdentidade(self):
+		return self.identidade
