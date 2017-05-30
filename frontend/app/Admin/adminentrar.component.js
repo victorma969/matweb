57b09676ccdbc199fd847ae3d688fc5ae88ba3b8
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-29 23:38:24 -0300

    lol 2017-05-29

--- frontend/app/Admin/adminentrar.component.js
+++ frontend/app/Admin/adminentrar.component.js
@@ -2,18 +2,19 @@ angular.
   module('Admin').
   component('adminEntrar', {
     templateUrl: '/app/Admin/entrar.template.html',
-    controller: ['ApiAdmin','$http','$location', 'MatWebGlobals',function Entrar(ApiAdmin,$http,$location,MatWebGlobals) {
+    controller: ['ApiAdmin','$http','$location', 'MatWebGlobals', '$scope',function Entrar(ApiAdmin,$http,$location,MatWebGlobals,$scope) {
       this.formulario = {'usuario':'','senha':''};
       this.entrar = function()
       {
-       	ApiAdmin.Entrar(this.formulario,function(resultado) {
+        ApiAdmin.Entrar(this.formulario,function(resultado) {
           MatWebGlobals.usuarioLogado = resultado.corpo.usuario;
-       		$http.defaults.headers.common.Authorization = resultado.corpo.token;
-       		window.localStorage.setItem('token_de_acesso', resultado.corpo.token);
-       		$location.path('/Admin')
-   		}, function(erro){
-   			this.erro = erro.data.mensagem
-   		} );
-   	  }
+          $http.defaults.headers.common.Authorization = resultado.corpo.token;
+          window.localStorage.setItem('token_de_acesso', resultado.corpo.token);
+          $location.path('/Usuario/Home')
+      }, function(error){
+            $scope.erro = error.data.mensagem;
+            $scope.$digest();
+      } );
+      }
     }]
-  });
+});
--- frontend/app/Menuprincipaladmin/tela-principal2.component.js
+++
@@ -1,8 +0,0 @@
-angular.
-  module('nucleoadmin').
-  component('telaPrincipal', {
-    templateUrl: '/app/Menuprincipaladmin/tela-principal.template.html',
-    controller: ['MatWebGlobals', function Menu(MatWebGlobals) {
-      
-    }]
-  });
