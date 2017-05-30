c8e29fe96389ac48b530ad7ea33058dde756e220
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-29 23:46:02 -0300

    lol 2017-05-29

--- frontend/app/Menuprincipaladmin/menu-principal2.component.js
+++ frontend/app/Menuprincipaladmin/menu-principal2.component.js
@@ -2,11 +2,25 @@ angular.
   module('nucleoadmin').
   component('adminUsuario', {
     templateUrl: '/app/Menuprincipaladmin/index.html',
-    controller: ['MatWebGlobals', function Menu(MatWebGlobals) {
-      if (typeof MatWebGlobals.usuarioLogado !== 'undefined') {
-        this.perfil = MatWebGlobals.usuarioLogado.perfil;
-      }else{
-        this.perfil = ""
+    controller: ['ApiMenuAdmin', 'MatWebGlobals', '$scope', '$location', function Entrar(ApiMenuAdmin,MatWebGlobals,$scope,$location) {
+        if (MatWebGlobals.hasOwnProperty('usuarioLogado')) {
+            $scope.nomeUsuario = MatWebGlobals.usuarioLogado.nome;
+            $scope.cpfUsuario = MatWebGlobals.usuarioLogado.cpf;
+        } else {
+            $location.path('/Usuario/Entrar');
+        }
+        
+  var ctrl = this;
+  ctrl.usuarios = [];
+        this.pesquisar = function()
+      {
+        ApiMenuAdmin.Listar({},function(resultado) {
+              ctrl.usuarios = resultado.corpo
+      console.log(ctrl.usuarios)
+    }, function(erro){
+        ctrl.erro = erro.data.mensagem
+      console.log(ctrl.erro)
+      } );
       }
     }]
-  });
+});
