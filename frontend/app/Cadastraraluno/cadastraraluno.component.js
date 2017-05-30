e68435808e67aa498950f3bf46c776fd5af34ba2
Gabriel <gabryelll.carvalho@gmail.com>
2017-05-30 11:25:19 -0300

    adicionando mais campos no pedido de cadastro

--- frontend/app/Cadastraraluno/cadastraraluno.component.js
+++ frontend/app/Cadastraraluno/cadastraraluno.component.js
@@ -4,14 +4,16 @@ angular.
     templateUrl: '/app/Cadastraraluno/cadastraraluno.template.html',
     controller: ['ApiUsuarioCadastrar','$http','$location', 'MatWebGlobals',function(ApiUsuarioCadastrar,$http,$location,MatWebGlobals) {
       var ctrl = this;
-      this.formulario = {'nome':'','matricula':'','cpf':'','perfil':'','senha':''};
+      this.formulario = {'nome':'','matricula':'','cpf':'','perfil':'','email':'','sexo':'','nome_pai':'','nome_mae':'','ano_conclusao':'','identidade':'','senha':''};
       this.cadastrar = function()
       {
           ApiUsuarioCadastrar.Cadastrar(ctrl.formulario,function(data){
             ctrl.mensagem = "Usuário cadastrado com sucesso";
+          
           },function(data){
             ctrl.mensagem = "ERRO";
           });
     };
     }]
 });
+  
