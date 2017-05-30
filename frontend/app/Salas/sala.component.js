978567dc4d3a85195a910fb5747858095e9f99e2
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-30 00:28:17 -0300

    lol 2017-05-30

--- frontend/app/Salas/sala.component.js
+++ frontend/app/Salas/sala.component.js
@@ -2,15 +2,15 @@ angular.
   module('Sala').
   component('listarSalas', {
     templateUrl: '/app/Salas/sala.template.html',
-    controller: ['ApiSalasApiSalasApiSalas', 'MatWebGlobals',function Entrar(ApiSalasApiSalas,MatWebGlobals) {
-      this.nome_disciplina = "";
+    controller: ['ApiSalas', 'MatWebGlobals',function Entrar(ApiSalas,MatWebGlobals) {
+      this.nome_sala = "";
 	var ctrl = this;
-	ctrl.disciplinas = [];
+	ctrl.salas = [];
       this.pesquisar = function()
       {
-       	ApiSalas.Listar({id_departamento: 95, nome: ctrl.nome_disciplina, pagina: 0, quantidade: 1000 },function(resultado) {
-		          ctrl.disciplinas = resultado.corpo
-			console.log(ctrl.disciplinas)
+       	ApiSalas.Listar({'id_predio':'','codigo':'', pagina: 0, quantidade: 1000 },function(resultado) {
+		          ctrl.salas = resultado.corpo
+			console.log(ctrl.salas)
 		}, function(erro){
    			ctrl.erro = erro.data.mensagem
 			console.log(ctrl.erro)
