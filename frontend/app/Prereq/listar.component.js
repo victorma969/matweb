3fb193e703f39be0bfe3d8b38429f8196babccdc
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-01 01:27:53 -0300

    ajuste formulario

--- frontend/app/Prereq/listar.component.js
+++ frontend/app/Prereq/listar.component.js
@@ -3,7 +3,7 @@ angular.
   component('prereqListar', {
     templateUrl: '/app/Prereq/listar.template.html',
     controller: ['ApiPrereq', 'MatWebGlobals','$scope', function Listar(ApiPrereq,MatWebGlobals,$scope) {
-      this.formulario = {'id_departamento':'','nome':'','pagina':'','quantidade':''};
+      this.formulario = {'disciplina':'','pagina':'','quantidade':''};
       this.listar = function()
       {
        	ApiDisciplina.Listar(this.formulario,function(resultado) {
