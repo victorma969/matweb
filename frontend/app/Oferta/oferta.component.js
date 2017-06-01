735d67fcc3d294f54a0124143a476ebf97edf46f
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-31 22:50:14 -0300

    lol 2017-05-31

--- frontend/app/Oferta/oferta.component.js
+++ frontend/app/Oferta/oferta.component.js
@@ -4,19 +4,6 @@ angular.
     templateUrl: '/app/Oferta/oferta.template.html',
     controller: ['ApiOferta', 'MatWebGlobals',function Entrar(ApiOferta,MatWebGlobals) {
       this.nome_disciplina = "";
-
-      
-
-
-it('should check ng-click', function() {
-  expect(element(by.binding('count')).getText()).toMatch('0');
-  element(by.css('button')).click();
-  expect(element(by.binding('count')).getText()).toMatch('1');
-});
-
-
-
-
 	var ctrl = this;
 	ctrl.disciplinas = [];
       this.pesquisar = function()
