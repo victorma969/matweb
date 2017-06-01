0b001ef0de63817b844e7350d442442491c4bac5
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-31 22:53:33 -0300

    lol 2017-05-31

--- frontend/app/Exibir/exibir.component.js
+++ frontend/app/Exibir/exibir.component.js
@@ -3,15 +3,6 @@ angular.
   component('materiaInfo', {
     templateUrl: '/app/Exibir/exibir.template.html',
     controller: ['ApiExibir', 'MatWebGlobals',function Entrar(ApiExibir,MatWebGlobals) {
-      
-
-it('should check ng-click', function() {
-  expect(element(by.binding('count')).getText()).toMatch('0');
-  element(by.css('button')).click();
-  expect(element(by.binding('count')).getText()).toMatch('1');
-});
-
-
       this.nome_disciplina = "";
   var ctrl = this;
   ctrl.disciplina = [];
