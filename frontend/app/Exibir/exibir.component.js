d87461093a979cf21164ee867c5d8354455a39e6
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-05-31 22:39:23 -0300

    lol 2017-05-31

--- frontend/app/Exibir/exibir.component.js
+++ frontend/app/Exibir/exibir.component.js
@@ -3,21 +3,14 @@ angular.
   component('materiaInfo', {
     templateUrl: '/app/Exibir/exibir.template.html',
     controller: ['ApiExibir', 'MatWebGlobals',function Entrar(ApiExibir,MatWebGlobals) {
-if (MatWebGlobals.hasOwnProperty('usuarioOferta')) {
-            $scope.idMateria = MatWebGlobals.usuarioOferta.id;
-            $scope.codigoMateria = MatWebGlobals.usuarioOferta.codigo;
-        } else {
-            $location.path('/Sexo');
-        }
       this.nome_disciplina = "";
   var ctrl = this;
-  ctrl.disciplinas = [];
+  ctrl.disciplina = [];
       this.pesquisar = function()
       {
-        ApiExibir.Listar(this.formulario,function(resultado) {
-          MatWebGlobals.usuarioOferta = resultado.corpo.oferta;
-          $http.defaults.headers.common.Authorization = resultado.corpo.token;
-      console.log(ctrl.disciplinas)
+        ApiExibir.Listar({id_departamento: 95, nome: ctrl.nome_disciplina, pagina: 0, quantidade: 1000 },function(resultado) {
+              ctrl.disciplina = resultado.corpo
+      console.log(ctrl.disciplina)
     }, function(erro){
         ctrl.erro = erro.data.mensagem
       console.log(ctrl.erro)
