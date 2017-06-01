a7aed3a4c0285dba42756ce2d441824fbde0af27
FelipeAires <felipeaires2008@gmail.com>
2017-06-01 03:43:42 -0300

    opa

--- frontend/app/Oferta/oferta.component.js
+++ frontend/app/Oferta/oferta.component.js
@@ -4,12 +4,11 @@ angular.
     templateUrl: '/app/Oferta/oferta.template.html',
     controller: ['ApiOferta', 'MatWebGlobals',function Entrar(ApiOferta,MatWebGlobals) {
       this.nome_disciplina = "";
-			this.id = "";
 	var ctrl = this;
 	ctrl.disciplinas = [];
       this.pesquisar = function()
       {
-       	ApiOferta.Listar({ id_departamento: ctrl.id , nome: ctrl.nome_disciplina, pagina: 0, quantidade: 1000 },function(resultado) {
+       	ApiOferta.Listar({id_departamento: 95 , nome: ctrl.nome_disciplina, pagina: 0, quantidade: 1000 },function(resultado) {
 		          ctrl.disciplinas = resultado.corpo
 			console.log(ctrl.disciplinas)
 		}, function(erro){
