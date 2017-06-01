b44b6789235b1c38aaa775b59a3e5d7de661ac44
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-01 01:08:44 -0300

    ajuste model

--- backend/Models/Prereq/Prereq.py
+++ backend/Models/Prereq/Prereq.py
@@ -1,5 +1,6 @@
 class Prereq(object):
-	def __init__(self,prereq):
-		self.id = prereq.getId()
-		self.grupo = prereq.getGrupo()
-                self.id_disc_pre = prereq.getId_disc_pre()
+	def __init__(self,dados=None):
+		if dados is not None:
+			self.disciplina = dados['nome']
+			self.prereq = dados['prereq']
+	      self.grupo = dados['grupo']
